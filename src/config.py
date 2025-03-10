8aef506a9731000a8a57f84965167c5192fa4be5
Khalid Sabban <khalidsaban05@gmail.com>
2025-03-10 15:41:02 +0000

    edit files paths

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
-CONFIG = {
-    'file_path': 'spotify_songs.csv',
-    'train_path': 'train.csv',
-    'test_path': 'test.csv',
+split_data_config = {
+    'file_path': '../data/raw/spotify_songs.csv',  # Ensure this points to the actual CSV file
+    'train_path': '../data/processed/train.csv',
+    'test_path': '../data/processed/test.csv',
     'test_size': 0.2,
     'random_state': 42
 }
