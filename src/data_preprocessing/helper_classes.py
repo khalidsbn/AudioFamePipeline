451def1df1bcefa735328bb9a2f000a792fd4f92
Khalid Sabban <khalidsaban05@gmail.com>
2025-04-21 18:32:25 +0100

    all classes for processing data

---
+++ src/data_preprocessing/helper_classes.py
@@ -0,0 +1,92 @@
+import re
+import nltk
+import emoji
+import unicodedata
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from textblob import TextBlob
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+import warnings
+warnings.filterwarnings('ignore')
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+class DropNaN(BaseEstimator, TransformerMixin):
+  def fit(self, X, y=None):
+    return self
+  def transform(self, X):
+    return X.dropna(axis=0, how='any')
+  
+class TextProcessing(BaseEstimator, TransformerMixin):
+  def __init__(self, column_name='playlist_name', feature='playlist'):
+    self.column_name = column_name
+    self.processed_column_name = f'processed_{column_name}'
+    self.feature = feature
+
+  def fit(self, X, y=None):
+    return self
+
+  def normalize_text(self, text):
+    # if text is null or not a string, return empty string
+    if pd.isna(text) or not isinstance(text, str):
+      return ""
+    text = text.lower()
+    return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
+
+  def clean_text(self, text):
+    # Remove URLs
+    text = re.sub(r'http\S+', '', text)
+    # Replace emojis with their textual description
+    text = emoji.replace_emoji(text, replace='')
+    # Remove special characters (keep letters, numbers, and spaces)
+    text = re.sub(r'[^a-z0-9\s]', '', text)
+    # Collabse multiple space
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+  def tokenize_and_remove_stopwords(self, text):
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(text)
+    return ' '.join([token for token in tokens if token not in stop_words])
+
+  def lemmatize_text(self, text):
+    lemmatizer = WordNetLemmatizer()
+    tokens = word_tokenize(text)
+    return ' '.join([lemmatizer.lemmatize(token) for token in tokens])
+
+  def transform(self, X):
+    X = X.copy()
+    X[self.processed_column_name] = X[self.column_name].apply(self.normalize_text)
+    X[self.processed_column_name] = X[self.processed_column_name].apply(self.clean_text)
+    X[self.processed_column_name] = X[self.processed_column_name].apply(self.tokenize_and_remove_stopwords)
+    X[self.processed_column_name] = X[self.processed_column_name].apply(self.lemmatize_text)
+    # Feature name prefix
+    feature_prefix = self.feature
+    X[f'{feature_prefix}_length'] = X[self.processed_column_name].apply(len)
+    X[f'{feature_prefix}_word_count'] = X[self.processed_column_name].apply(lambda x: len(x.split()))
+    X[f'{feature_prefix}_sentiment'] = X[self.processed_column_name].apply(lambda x: TextBlob(x).sentiment.polarity)
+    return X.drop(self.column_name, axis=1)
+  
+class DateProcessing(BaseEstimator, TransformerMixin):
+  def fit(self, X, y=None):
+    return self
+
+  def transform(self, X):
+    X = X.copy()
+    X["track_album_release_date"] = pd.to_datetime(X["track_album_release_date"], errors="coerce")
+    X["track_album_release_year"] = X["track_album_release_date"].dt.year.astype("Int64")
+    X["track_album_release_month"] = X["track_album_release_date"].dt.month.astype("Int64")
+    X["track_album_release_day"] = X["track_album_release_date"].dt.day.astype("Int64")
+    return X.drop('track_album_release_date', axis=1)
