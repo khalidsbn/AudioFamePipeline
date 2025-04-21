2bd5e582b17f2c58c277c68ea4f873a8e99df785
Khalid Sabban <khalidsaban05@gmail.com>
2025-04-21 18:33:31 +0100

    process data

---
+++ src/data_preprocessing/clean_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from helper_classes import DropNaN, TextProcessing, DateProcessing
+
+# Load your dataset
+df = pd.read_csv('../../data/processed/train.csv')
+
+# Group features
+track_metadata = [
+    'track_id', 'track_name', 'track_artist', 'track_popularity',
+    'track_album_id', 'track_album_name', 'track_album_release_date'
+]
+
+playlist_metadata = [
+     'track_id', 'playlist_name', 'playlist_id', 'playlist_genre', 'playlist_subgenre'
+]
+
+audio_features = [
+    'track_id', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness',
+    'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'track_popularity'
+]
+
+# Create separate DataFrames for each group
+df_track = df[track_metadata]
+df_playlist = df[playlist_metadata]
+df_audio = df[audio_features]
