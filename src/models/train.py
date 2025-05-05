4c509846a97ee60ddd6d03ed6d37656ae169f9f7
Khalid Sabban <khalidsaban05@gmail.com>
2025-05-05 18:25:26 +0100

    traning script

--- src/models/train.py
+++ src/models/train.py
@@ -1,60 +1,39 @@
+import joblib
 import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.base import BaseEstimator, TransformerMixin
+from xgboost import XGBRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 
-df_audio = pd.read_csv('../../data/audio_data_clean.csv')
-df_playlist = pd.read_csv('../../data/playlist_data_clean.csv')
-df_track = pd.read_csv('../../data/track_data_clean.csv')
+df = pd.read_csv('../../data/features/data_cleaned.csv')
 
-class MergeTrackData(BaseEstimator, TransformerMixin):
-    def __init__(self, df_playlist, df_track):
-        self.df_playlist = df_playlist
-        self.df_track = df_track
+X = df.drop(columns=['track_popularity'])
+y = df['track_popularity']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-    def fit(self, X, y=None):
-        return self
+# Regression Model
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred_linear = model.predict(X_test)
 
-    def transform(self, X):
-        # Assume X is df_audio
-        df_playlist_unique = self.df_playlist.drop_duplicates(subset='track_id')
-        df_track_unique = self.df_track.drop_duplicates(subset='track_id')
+# Random Forest Model
+forest = RandomForestRegressor(random_state=42)
+forest.fit(X_train, y_train)
+y_pred_rf = forest.predict(X_test)
 
-        df_merged = (
-            pd.merge(X, df_playlist_unique, on='track_id')
-              .merge(df_track_unique, on='track_id')
-        )
-        return df_merged
+# XGBoost Model
+xgb_params = {'max_depth': 20,
+              'eta':0.1,
+              'subsample': 0.8,
+              'colsample_bytree': 0.6,
+              'seed': 2018,
+              'objective': 'reg:squarederror'}
+xgb = XGBRegressor(**xgb_params)
+xgb.fit(X, y)
+y_pred_xgb = xgb.predict(X_test)
 
-class FixTrackPopularityColumn(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X = X.drop(columns=['track_popularity_x', 'track_id', 'track_album_id', 'playlist_id'])
-        X.rename(columns={'track_popularity_y': 'track_popularity'}, inplace=True)
-        return X
-
-merge_pipeline = Pipeline([
-    ('merge_tracks', MergeTrackData(df_playlist, df_track)),
-    ('fix_track_popularity', FixTrackPopularityColumn()),
-])
-
-df = merge_pipeline.fit_transform(df_audio)
-
-num_cols = df.select_dtypes(include=['number']).columns.tolist()
-str_cols = df.select_dtypes(include=['object']).columns.tolist()
-
-from sklearn.preprocessing import OrdinalEncoder
-class Encoder(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        ordinal_encoder = OrdinalEncoder()
-        X[self.columns] = ordinal_encoder.fit_transform(X[self.columns])
-        return X
-    
-df[str_cols] = Encoder(str_cols).fit_transform(df[str_cols])
+# Save the training models
+joblib.dump(model, '../../models/regression_model.pkl')
+joblib.dump(forest, '../../models/forest_model.pkl')
+joblib.dump(xgb, '../../models/xgb_model.pkl')
+print("Models saved successfully!")
